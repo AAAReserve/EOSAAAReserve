d1856cbe049e310de62cf9144a4bd2be00609c33
I'm The Cheese <ch33s3codes@gmail.com>
2019-01-03 18:36:53 +0300

    ch/Restructure token implementation. WIP

---
+++ reservetoken/v2/reservetoken.cpp
@@ -0,0 +1,121 @@
+#include "reservetoken.hpp"
+
+namespace eosio
+{
+ACTION reservetoken::create(name issuer, asset maximum_supply)
+{
+    require_auth(_self);
+
+    auto sym = maximum_supply.symbol;
+    eosio_assert(sym.is_valid(), "invalid symbol name");
+    eosio_assert(maximum_supply.is_valid(), "invalid supply");
+    eosio_assert(maximum_supply.amount > 0, "max-supply must be positive");
+
+    stats statstable(_self, sym.name());
+    auto existing = statstable.find(sym.name());
+    eosio_assert(existing == statstable.end(), "token with symbol already exists");
+
+    statstable.emplace(_self, [&](auto &s) {
+        s.supply.symbol = maximum_supply.symbol;
+        s.max_supply = maximum_supply;
+        s.issuer = issuer;
+    });
+}
+
+ACTION reservetoken::issue(name to, asset quantity, string memo)
+{
+    auto sym = quantity.symbol;
+    eosio_assert(sym.is_valid(), "invalid symbol name");
+    eosio_assert(memo.size() <= 256, "memo has more than 256 bytes");
+
+    stats statstable(_self, sym.code().raw());
+    auto existing = statstable.find(sym.code().raw());
+    eosio_assert(existing != statstable.end(), "token with symbol does not exist, create token before issue");
+    const auto &st = *existing;
+
+    require_auth(st.issuer);
+    eosio_assert(quantity.is_valid(), "invalid quantity");
+    eosio_assert(quantity.amount > 0, "must issue positive quantity");
+
+    eosio_assert(quantity.symbol == st.supply.symbol, "symbol precision mismatch");
+    eosio_assert(quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
+
+    statstable.modify(st, same_payer, [&](auto &s) {
+        s.supply += quantity;
+    });
+
+    add_balance(st.issuer, quantity, st.issuer);
+
+    if (to != st.issuer)
+    {
+        SEND_INLINE_ACTION(*this, transfer, {{st.issuer, "active"_n}},
+                           {st.issuer, to, quantity, memo});
+    }
+}
+
+ACTION reservetoken::burn(asset quantity)
+{
+    auto sym = quantity.symbol;
+    eosio_assert(sym.is_valid(), "invalid symbol name");
+
+    stats statstable(_self, sym.code().raw());
+    auto existing = statstable.find(sym.code().raw());
+    eosio_assert(existing != statstable.end(), "token with symbol does not exist");
+    const auto &st = *existing;
+
+    require_auth(st.issuer);
+    eosio_assert(quantity.is_valid(), "invalid quantity");
+    eosio_assert(quantity.amount > 0, "must retire positive quantity");
+
+    eosio_assert(quantity.symbol == st.supply.symbol, "symbol precision mismatch");
+
+    statstable.modify(st, same_payer, [&](auto &s) {
+        s.supply -= quantity;
+    });
+
+    sub_balance(st.issuer, quantity);
+}
+
+ACTION treservetoken::ransfer(name from, name to, asset quantity, string memo)
+{
+    eosio_assert(from != to, "cannot transfer to self");
+    require_auth(from);
+    eosio_assert(is_account(to), "to account does not exist");
+    auto sym = quantity.symbol.code();
+    stats statstable(_self, sym.raw());
+    const auto &st = statstable.get(sym.raw());
+
+    require_recipient(from);
+    require_recipient(to);
+
+    eosio_assert(quantity.is_valid(), "invalid quantity");
+    eosio_assert(quantity.amount > 0, "must transfer positive quantity");
+    eosio_assert(quantity.symbol == st.supply.symbol, "symbol precision mismatch");
+    eosio_assert(memo.size() <= 256, "memo has more than 256 bytes");
+
+    auto payer = has_auth(to) ? to : from;
+
+    sub_balance(from, quantity);
+    add_balance(to, quantity, payer);
+}
+
+ACTION reservetoken::setmaxsupply(asset maximum_supply)
+{
+    auto sym = quantity.symbol;
+    eosio_assert(sym.is_valid(), "invalid symbol name");
+    auto sym_name = sym.name();
+    stats statstable(_self, sym_name);
+    auto existing = statstable.find(sym_name);
+    eosio_assert(existing != statstable.end(), "token with symbol does not exist, create token before issue");
+    const auto &stat = *existing;
+    require_auth(stat.issuer);
+    eosio_assert(quantity.is_valid(), "invalid quantity");
+    eosio_assert(quantity.amount > 0, "must issue positive quantity");
+    eosio_assert(quantity.symbol == stat.supply.symbol, "symbol precision mismatch");
+
+    statstable.modify(stat, 0, [&](auto &s) {
+        s.max_supply = quantity;
+    });
+}
+
+} // namespace eosio
